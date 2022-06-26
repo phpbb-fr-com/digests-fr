fc27bbc08784b6692be4a8aab22516ae9f2b13d2
PhilippeGanter <phg@e-node.com>
2022-06-26 18:19:33 +0200

    Ajout des espaces insécables

--- acp/info_acp_common.php
+++ acp/info_acp_common.php
@@ -30,7 +30,7 @@ if (empty($lang) || !is_array($lang))
 // dans une url, il n'est pas nécessaire de spécifier un ordre, par exemple "Cliquez sur %ICI%s" est suffisant
 //
 // Quelques caractères que vous pouvez copier/coller :
-// ’ » “ ” …
+// ’ « » “ ” …
 //
 
 $lang = array_merge($lang, array(
